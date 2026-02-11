a6ff78012e9bd95f599f46252d56207290757f7f
ahassh1 <ahasanhabibpolash420@gmail.com>
2026-02-11 13:48:22 +0600

    load + display lesson with API

--- script/index.js
+++ script/index.js
@@ -1,1 +1,22 @@
-console.log("this is js project");
+const loadLessons = () => {
+  fetch("https://openapi.programming-hero.com/api/levels/all") //promise of response
+    .then((res) => res.json()) //promise of json data
+    .then((json) => displayLessons(json.data));
+};
+loadLessons();
+
+const displayLessons = (lessons) => {
+  // 1. get the container
+  const levelContainer = document.getElementById("level-container");
+  levelContainer.innerHTML = "";
+  //2. get into every lessons
+  for (let lesson of lessons) {
+    //3. create element (createElement)
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
+      <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book"></i>Lesson- ${lesson.level_no}</button>
+    `;
+    //4. append into container (appendChild)
+    levelContainer.appendChild(btnDiv);
+  }
+};
